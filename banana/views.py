bc154cafa4f16d6c522dce30ac149bf7b2d15437
adelakim <diddnjs02@likelion.org>
2020-01-06 23:12:09 +0900

    testskill two

--- banana/views.py
+++ banana/views.py
@@ -11,17 +11,20 @@ from .serializers import PostSerializer
 @csrf_exempt
 def hello(request):
     received_json_data = json.loads(request.body.decode("utf-8"))
-    print(received_json_data['userRequest']['utterance'])
+    utterance = received_json_data['userRequest']['utterance']
+    param_id = int(received_json_data['action']['params']['id'])
+    # print(type(param_id))
+    post = Post.objects.all().get(id=param_id)
     return JsonResponse({
-  "version": "2.0",
-  "template": {
-    "outputs": [{
-      "simpleText": {
-        "text": "hello I'm Ryan"
-      }
-    }]
-  }
-})
+        "version": "2.0",
+        "template": {
+            "outputs": [{
+            "simpleText": {
+                "text":"id : {} , title : {} , content : {}".format(param_id, post.title, post.content)
+            }
+            }]
+        }
+    })
 
 # Create your views he`re.
 class PostViewSet(viewsets.ModelViewSet):
