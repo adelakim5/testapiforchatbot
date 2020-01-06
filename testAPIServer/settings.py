94b5a0c2b67dbf1550f683c6cddb9a026a8ab06a
adelakim <diddnjs02@likelion.org>
2020-01-06 19:06:12 +0900

    modify settings.py

--- testAPIServer/settings.py
+++ testAPIServer/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '14=z+w@5rerty3&c0&o*%&t*er*k=jit4tg_6usow+t+5^$c*l'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '14=z+w@5rerty3&c0&o*%&t*er*k=jit4tg_6usow+t+5^$c*l')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
