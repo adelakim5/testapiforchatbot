c81625e4e6062e554c90a9279d98e71841b4c62e
adelakim <diddnjs02@likelion.org>
2020-01-06 20:36:05 +0900

    first commit

--- testAPIServer/settings.py
+++ testAPIServer/settings.py
@@ -126,9 +126,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'banana','static')
-)
+]
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
