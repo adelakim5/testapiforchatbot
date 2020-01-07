308c15af965f1fdb0a54fcb326a72efbc75baad5
adelakim <diddnjs02@likelion.org>
2020-01-07 13:16:20 +0900

    testSkill fifth

--- banana/views.py
+++ banana/views.py
@@ -10,59 +10,23 @@ from .serializers import PostSerializer
 
 @csrf_exempt
 def hello(request):
-    received_json_data = json.loads(request.body.decode("utf-8"))
-    print(received_json_data)
-    utterance = received_json_data['userRequest']['utterance']
-    param_id = int(received_json_data['action']['params']['id'])
+    # received_json_data = json.loads(request.body.decode("utf-8"))
+    # print(received_json_data)
+    # utterance = received_json_data['userRequest']['utterance']
+    # param_id = int(received_json_data['action']['params']['id'])
     # print(type(param_id))
-    post = Post.objects.all().get(id=param_id)
+    # post = Post.objects.all().get(id=param_id)
     return JsonResponse({
-  "version": "2.0",
-  "template": {
-    "outputs": [
-      {
-        "listCard": {
-          "header": {
-            "title": "카카오 i 디벨로퍼스를 소개합니다",
-            "imageUrl": "http://k.kakaocdn.net/dn/xsBdT/btqqIzbK4Hc/F39JI8XNVDMP9jPvoVdxl1/2x1.jpg"
-          },
-          "items": [
+    "version": "2.0",
+    "template": {
+        "outputs": [
             {
-              "title": "Kakao i Developers",
-              "description": "새로운 AI의 내일과 일상의 변화",
-              "imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
-              "link": {
-                "web": "https://namu.wiki/w/%EB%9D%BC%EC%9D%B4%EC%96%B8(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
-              }
-            },
-            {
-              "title": "Kakao i Open Builder",
-              "description": "카카오톡 채널 챗봇 만들기",
-              "imageUrl": "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
-              "link": {
-                "web": "https://namu.wiki/w/%EB%AC%B4%EC%A7%80(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
-              }
-            },
-            {
-              "title": "Kakao i Voice Service",
-              "description": "보이스봇 / KVS 제휴 신청하기",
-              "imageUrl": "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
-              "link": {
-                "web": "https://namu.wiki/w/%EC%96%B4%ED%94%BC%EC%B9%98"
-              }
+                "simpleText": {
+                    "text": "간단한 텍스트 요소입니다."
+                }
             }
-          ],
-          "buttons": [
-            {
-              "label": "구경가기",
-              "action": "webLink",
-              "webLinkUrl": "https://namu.wiki/w/%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88"
-            }
-          ]
-        }
-      }
-    ]
-  }
+        ]
+    }
 })
 
 # Create your views he`re.
