773c1dd508240963f213663494d8fb5d681dd501
adelakim <diddnjs02@likelion.org>
2020-01-07 16:16:40 +0900

    add blockId

--- banana/views.py
+++ banana/views.py
@@ -12,6 +12,8 @@ from .serializers import PostSerializer
 def hello(request):
     received_json_data = json.loads(request.body.decode("utf-8"))
     print(received_json_data)
+    block_id = received_json_data['userRequest']['block']['id']
+    block_name = received_json_data['userRequest']['block']['name']
     # utterance = received_json_data['userRequest']['utterance']
     # param_id = int(received_json_data['action']['params']['id'])
     # print(type(param_id))
@@ -30,21 +32,25 @@ def hello(request):
             {
                 "messageText": "극히 드물다",
                 "action": "block",
+                "blockId": "5e1427ac8192ac00017938b5",
                 "label": "극히 드물다"
             },
             {
                 "messageText": "가끔 있었다",
                 "action": "block",
+                "blockId": "5e1427ac8192ac00017938b5",
                 "label": "가끔 있었다"
             },
             {
                 "messageText": "종종 있었다",
                 "action": "block",
+                "blockId": "5e1427ac8192ac00017938b5",
                 "label": "종종 있었다"
             },
             {
                 "messageText": "대부분 그랬다",
                 "action": "block",
+                "blockId": "5e1427ac8192ac00017938b5",
                 "label": "대부분 그랬다"
             }
             ]
