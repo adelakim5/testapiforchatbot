58e083a1601e5f6afd8e7a6b4241a5aa18cfe12a
adelakim <diddnjs02@likelion.org>
2020-01-07 12:55:24 +0900

    testSkill three

--- banana/views.py
+++ banana/views.py
@@ -11,20 +11,45 @@ from .serializers import PostSerializer
 @csrf_exempt
 def hello(request):
     received_json_data = json.loads(request.body.decode("utf-8"))
+    print(received_json_data)
     utterance = received_json_data['userRequest']['utterance']
     param_id = int(received_json_data['action']['params']['id'])
     # print(type(param_id))
     post = Post.objects.all().get(id=param_id)
     return JsonResponse({
-        "version": "2.0",
-        "template": {
-            "outputs": [{
-            "simpleText": {
-                "text":"id : {} , title : {} , content : {}".format(param_id, post.title, post.content)
-            }
-            }]
+"version": "2.0",
+  "template": {
+    "outputs": [
+      {
+        "simpleText": {
+          "text": "토트넘 선수 리스트입니다."
         }
-    })
+      }
+    ],
+    "quickReplies": [
+      {
+        "messageText": "손흥민",
+        "action": "message",
+        "label": "손흥민"
+      },
+      {
+        "messageText": "헤리케인",
+        "action": "message",
+        "label": "헤리케인"
+      },
+      {
+        "messageText": "에릭센",
+        "action": "message",
+        "label": "에릭센"
+      },
+      {
+        "messageText": "요리스",
+        "action": "message",
+        "label": "요리스"
+      }
+    ]
+  }
+})
 
 # Create your views he`re.
 class PostViewSet(viewsets.ModelViewSet):
