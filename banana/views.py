d3f3652951a9f7e183980f85ac668923e2267828
adelakim <diddnjs02@likelion.org>
2020-01-07 16:04:51 +0900

    print request.body

--- banana/views.py
+++ banana/views.py
@@ -10,46 +10,46 @@ from .serializers import PostSerializer
 
 @csrf_exempt
 def hello(request):
-    # received_json_data = json.loads(request.body.decode("utf-8"))
-    # print(received_json_data)
+    received_json_data = json.loads(request.body.decode("utf-8"))
+    print(received_json_data)
     # utterance = received_json_data['userRequest']['utterance']
     # param_id = int(received_json_data['action']['params']['id'])
     # print(type(param_id))
     # post = Post.objects.all().get(id=param_id)
     data = {
-"version": "2.0",
-  "template": {
-    "outputs": [
-      {
-        "simpleText": {
-          "text": "토트넘 선수 리스트입니다."
+        "version": "2.0",
+        "template": {
+            "outputs": [
+            {
+                "simpleText": {
+                "text": "평소에는 아무렇지도 않던 일들이 괴롭고 귀찮게 느껴졌다."
+                }
+            }
+            ],
+            "quickReplies": [
+            {
+                "messageText": "극히 드물다",
+                "action": "block",
+                "label": "극히 드물다"
+            },
+            {
+                "messageText": "가끔 있었다",
+                "action": "block",
+                "label": "가끔 있었다"
+            },
+            {
+                "messageText": "종종 있었다",
+                "action": "block",
+                "label": "종종 있었다"
+            },
+            {
+                "messageText": "대부분 그랬다",
+                "action": "block",
+                "label": "대부분 그랬다"
+            }
+            ]
         }
-      }
-    ],
-    "quickReplies": [
-      {
-        "messageText": "손흥민",
-        "action": "message",
-        "label": "손흥민"
-      },
-      {
-        "messageText": "헤리케인",
-        "action": "message",
-        "label": "헤리케인"
-      },
-      {
-        "messageText": "에릭센",
-        "action": "message",
-        "label": "에릭센"
-      },
-      {
-        "messageText": "요리스",
-        "action": "message",
-        "label": "요리스"
-      }
-    ]
-  }
-}
+    }
     return JsonResponse(data)
 
 # Create your views he`re.
