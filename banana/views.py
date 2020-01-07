1bef1fb23668cb690a9c0c681a1523d169aacc72
adelakim <diddnjs02@likelion.org>
2020-01-07 14:02:40 +0900

    testSkill change ascii False

--- banana/views.py
+++ banana/views.py
@@ -28,7 +28,7 @@ def hello(request):
             ]
         }
     }
-    return JsonResponse(data,json_dumps_params = {'ensure_ascii': True},safe=False)
+    return JsonResponse(data,json_dumps_params = {'ensure_ascii': False},safe=False)
 
 # Create your views he`re.
 class PostViewSet(viewsets.ModelViewSet):
