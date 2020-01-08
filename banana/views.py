dffa4b36365044ac2dad162af9e8710f539fc003
adelakim <diddnjs02@likelion.org>
2020-01-08 17:18:25 +0900

    add question and block

--- banana/views.py
+++ banana/views.py
@@ -11,7 +11,7 @@ from .mango.shared import BLOCK_ID
 ## Serializers
 from .serializers import PostSerializer
 
-ANSWER = ["극히 드물다","가끔 있었다","종종 있었다","대부분 그랬다","우울증 자가진단"]
+ANSWER = ["극히 드물다","가끔 있었다","종종 있었다","대부분 그랬다","우울증 자가진단 시작하기"]
 
 @csrf_exempt
 def hello(request):
